859cd1f10a87bcd419415acb4ff0a7b8e67312ee
Shane Lupton <slupton89@gmail.com>
2018-11-26 11:11:08 -0700

    basic login form

--- src/_containers/login-container.js
+++ src/_containers/login-container.js
@@ -6,17 +6,22 @@ require('./login-container.css');
 
 export function LoginForm(props) {
   return (
-    <form className='login-form' onSubmit={props.handleSubmit(values => {
-      console.log(values);
-    }
-    )} aria-label={'login form'}>
-      <Field name='username' id='username' component={'Input'} element='input' type='text'
-        label='Username' aria-label={'username field'} />
-      <Field name='password' id='password' component={'Input'} element='input' type='password'
-        label='Password' aria-label={'password field'}/>
+    <div id='login-section'>
+      <form className='login-form' onSubmit={props.handleSubmit(values => {
+        console.log(values);
+      }
+      )} aria-label={'login form'}>
+        <label for='username'>Username</label>
+        <Field name='username' id='username' component={'Input'} element='input' type='text'
+          aria-label={'username field'} />
+        <label for='username'>Password</label>
+        <Field name='password' id='password' component={'Input'} element='input' type='password'
+          aria-label={'password field'}/>
 
-      <button className='loginBtn' aria-label={'login submit'}>Submit</button>
-    </form>
+        <button className='loginBtn' aria-label={'login submit'}>Submit</button>
+      </form>
+    </div>
+
   )
 }
 
