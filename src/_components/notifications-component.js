8bb10a1a76ecea3145a06a8f123442e5242501cb
Shane Lupton <slupton89@gmail.com>
2018-11-29 09:53:17 -0700

    moved notifications to its own component, dispatches on new visits

---
+++ src/_components/notifications-component.js
@@ -0,0 +1,41 @@
+import {
+  actions as notifActions,
+  Notifs
+} from 'redux-notifications';
+import React from 'react';
+import { connect } from 'react-redux';
+import { clearNotif } from '../_actions/auth-action';
+
+const { notifSend } = notifActions;
+
+export class Notifications extends React.Component {
+  constructor() {
+    super(null);
+    window.NotifComponent = this;
+  }
+
+  send = () => {
+    if(this.props.loggedIn) {
+      this.props.dispatch(notifSend({
+        message: `Hello, ${this.props.user.name}`,
+        kind: 'info',
+        dismissAfter: 3000
+      }));
+      this.props.dispatch(clearNotif());
+    }
+  }
+
+  render() {
+    return (
+      <Notifs />
+    )
+  }
+
+}
+
+const mapStateToProps = state => ({
+  loggedIn: state.auth.notifLoggedIn,
+  user: state.auth.user,
+});
+
+export default connect(mapStateToProps)(Notifications);
