a1c8d3a8e848dd3d6958ccbcffa03d1b17a20e9a
Shane Lupton <slupton89@gmail.com>
2018-11-30 10:07:18 -0700

    styled login and sign up pages

--- src/_components/input-component.js
+++ src/_components/input-component.js
@@ -1,5 +1,25 @@
 import React from 'react';
+import styled from 'styled-components';
 
+const InputComponent = styled.nav`
+  .form-error {
+    font-size: 20px;
+    color: red;
+  }
+
+  #err-icon {
+    color: orangered;
+    font-size: 18px;
+    margin-right: 5px;
+  }
+
+  #warn-icon {
+    color: goldenrod;
+    font-size: 18px;
+    margin-right: 5px;
+  }
+
+`;
 export default class Input extends React.Component {
   componentDidUpdate(prevProps) {
     if (!prevProps.meta.active && this.props.meta.active) {
@@ -10,28 +30,40 @@ export default class Input extends React.Component {
   render() {
     let error;
     if (this.props.meta.touched && this.props.meta.error) {
-      error = <div className="form-error">{this.props.meta.error}</div>;
+      error = <div className="form-error">
+        <i class="fas fa-exclamation-triangle" id="err-icon"></i>
+        {this.props.meta.error}
+      </div>;
     }
 
     let warning;
     if (this.props.meta.touched && this.props.meta.warning) {
-      warning = <div className="form-warning">{this.props.meta.warning}</div>;
+      warning = <div className="form-warning">
+        <i class="fas fa-exclamation-triangle" id="warn-icon"></i>
+        {this.props.meta.warning}
+      </div>;
     }
 
     return (
-      <div className="form-input">
-        <label htmlFor={this.props.input.name}>
-          {this.props.label}
-          {error}
-          {warning}
-        </label>
-        <input
-          {...this.props.input}
-          id={this.props.input.name}
-          type={this.props.type}
-          ref={input => (this.input = input)}
-        />
-      </div>
+      <InputComponent>
+        <div className="form-input">
+          <label
+            htmlFor={this.props.input.name}
+
+          >
+            -{this.props.label}-
+            {error}
+            {warning}
+          </label>
+          <input
+            className="input-feedback"
+            {...this.props.input}
+            id={this.props.input.name}
+            type={this.props.type}
+            ref={input => (this.input = input)}
+          />
+        </div>
+      </InputComponent>
     );
   }
 }
